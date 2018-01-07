3137358a857dbde3c238dd7757837e3cb294a649
Neilbircumshaw <n.bircumshaw@hotmail.co.uk>
2018-01-07 23:43:03 +0000

    Project 8 - using Gulp
    Using gulp for the build process of a front end website

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+"use strict";
+
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const minCSS = require("gulp-clean-css");
+const maps = require("gulp-sourcemaps");
+const imagemin = require('gulp-imagemin');
+const del = require("del");
+
+// this task concatinates the 2 JS files together for better performance to a file called "glodal.js" in the js folder.
+
+gulp.task("concatScripts", function(){
+return gulp.src(["js/circle/autogrow.js","js/circle/circle.js"])
+.pipe(maps.init())
+.pipe(concat("global.js"))
+.pipe(maps.write("./"))
+.pipe(gulp.dest("js"));
+});
+
+/*This task minifies the JS global.js file into another renamed file called all.min.js using the rename method then
+it is stored in the dist directory under a sub-directory called scripts.
+I simply called the task "scripts" instead of minify scripts because it uses concatScripts as a dependancy anyway in
+its second argument and therfore does not really need another task that merges both the minify scripts and concat scripts as
+this already does that.
+*/
+gulp.task("scripts", ["concatScripts"], function(){
+return gulp.src("js/global.js")
+.pipe(uglify())
+.pipe(rename("all.min.js"))
+.pipe(gulp.dest("dist/scripts"));
+});
+
+//compiles the SASS files into CSS. Only need to use one file - "sass/global.scss" - as this exports all other SASS files.
+
+gulp.task("compileSass", function(){
+return gulp.src("sass/global.scss")
+.pipe(maps.init())
+.pipe(sass())
+.pipe(maps.write("./"))
+.pipe(gulp.dest("css"));
+})
+
+//Much like the "scripts" task, this task also minifies the CSS created from the "compileSass" task and uses it as a dependancy.
+
+gulp.task("styles", ["compileSass"], function(){
+return gulp.src("css/global.css")
+.pipe(minCSS())
+.pipe(rename("all.min.css"))
+.pipe(gulp.dest("dist/styles"));
+});
+
+// used globbing pattern to get all the jpg and png files in the images directory and optimise them and put in dist/content
+gulp.task("images", function() {
+return gulp.src(["images/*.jpg", "images/*.png"])
+.pipe(imagemin())
+.pipe(gulp.dest('dist/content'));
+});
+
+//deletes the folders and files listed that would be old and need refreshing
+
+gulp.task("clean", function() {
+del(["dist", "css","js/global.js","js/global.js.map"]);
+});
+
+//the watch task runs when any changes are made the the source sass files and then is compiled and minified with the styles task
+
+gulp.task("watchSass", function(){
+gulp.watch("sass/circle/**/*scss", ["styles"]);
+});
+
+/*the build task runs the clean task first and adds the html file to the dist folder as well as running the
+scripts,styles and images task to put the appropriate files into the dist folder, ready for deployment.
+*/
+gulp.task("build", ["clean"], function(){
+gulp.src("index.html")
+.pipe(gulp.dest("dist"));
+gulp.start(["scripts","styles","images"]);
+
+});
+
+/*the default task runs by typing "gulp" which will firstly run the "watchSass" task for any changes
+the the src code, then it will run the build task, which completes the build process.
+ */
+gulp.task("default", ["watchSass"], function(){
+gulp.start("build");
+});
